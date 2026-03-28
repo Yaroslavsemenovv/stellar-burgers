f673e2a40a64c96c920bc3801943bfc7d9100b61
Yaroslav Semenov <yaroslavsemenov100@gmail.com>
2026-03-29 00:14:11 +0300

    base

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,32 +1,121 @@
-import { ConstructorPage } from '@pages';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import {
+  AppHeader,
+  Protected,
+  Modal,
+  FeedOrderModalRoute,
+  OrderInfo,
+  IngredientDetails
+} from '@components';
 import { Preloader } from '@ui';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  NotFound404,
+  Profile,
+  ProfileOrders
+} from '@pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { useEffect } from 'react';
+import { getIngredients } from '../../services/burgerIngredients/actions';
+import {
+  selectIsLoading,
+  selectError
+} from '../../services/burgerIngredients/slice';
+
+import { getUser } from '../../services/userData/action';
 
 const App = () => {
-  /** TODO: взять переменные из стора */
-  const isIngredientsLoading = false;
-  const ingredients = [];
-  const error = null;
+  // запрос массива ингридиентов в юзэффекте диспатчится экшен на запрос массива ингридиентов,
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const isLoading = useSelector(selectIsLoading);
+  const errorMessage = useSelector(selectError);
+
+  const background = location.state?.background;
+  const onCloseFn = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    dispatch(getIngredients());
+    dispatch(getUser());
+  }, [dispatch]);
 
   return (
     <div className={styles.app}>
       <AppHeader />
-      {isIngredientsLoading ? (
-        <Preloader />
-      ) : error ? (
-        <div className={`${styles.error} text text_type_main-medium pt-4`}>
-          {error}
-        </div>
-      ) : ingredients.length > 0 ? (
-        <ConstructorPage />
-      ) : (
-        <div className={`${styles.title} text text_type_main-medium pt-4`}>
-          Нет игредиентов
-        </div>
-      )}
+
+      {errorMessage && <div>{errorMessage}</div>}
+      {isLoading && <Preloader />}
+      <>
+        <Routes location={background || location}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/feed/:number' element={<OrderInfo />} />
+          <Route path='/ingredients/:id' element={<IngredientDetails />} />
+          {/* защищенный */}
+          <Route
+            path='/login'
+            element={<Protected onlyUnAuth component={<Login />} />}
+          />
+          <Route
+            path='/register'
+            element={<Protected onlyUnAuth component={<Register />} />}
+          />
+          <Route
+            path='/forgot-password'
+            element={<Protected onlyUnAuth component={<ForgotPassword />} />}
+          />
+          <Route
+            path='/reset-password'
+            element={<Protected onlyUnAuth component={<ResetPassword />} />}
+          />
+          <Route
+            path='/profile'
+            element={<Protected component={<Profile />} />}
+          />
+          <Route
+            path='/profile/orders'
+            element={<Protected component={<ProfileOrders />} />}
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={<Protected component={<OrderInfo />} />}
+          />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+        {background && (
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={<FeedOrderModalRoute onClose={onCloseFn} />}
+            />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <Modal title='Детали ингредиента' onClose={onCloseFn}>
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={<FeedOrderModalRoute onClose={onCloseFn} />}
+            />
+          </Routes>
+        )}
+      </>
     </div>
   );
 };
